2b8bf8e13202f278ed1d807e037bd202f735706c
unknown <tmkhiem@ldn-pc>
2026-07-12 20:18:08 +0700

    Add GH Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/InteractiveLearnNetL2L3/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-})
+}))
